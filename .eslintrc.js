ced10982951e5a9d6f26d59df120ee12b11e9f0d
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-06-06 21:05:46 +0200

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,60 @@ module.exports = {
 	rules: {
 		// Disable React import.
 		'react/react-in-jsx-scope': 0,
+		// Sort imports.
+		'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					['external', 'builtin'],
+					'internal',
+					['sibling', 'parent'],
+					'index',
+				],
+				pathGroups: [
+					{
+						pattern: '@(react|react-native)',
+						group: 'external',
+						position: 'before',
+					},
+					{
+						pattern: 'src/**',
+						group: 'internal',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['internal', 'react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+			},
+		],
+		// Allow use of arrow functions.
+		'arrow-body-style': ['warn', 'always'],
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: ['function-declaration', 'arrow-function'],
+				unnamedComponents: 'arrow-function',
+			},
+		],
+		// Disable extraneous dependencies for tests and specific files.
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				devDependencies: [
+					// Repos with a single test file.
+					'test.{ts,tsx}',
+					// Repos with multiple top-level test files.
+					'test-*.{ts,tsx}',
+					// Tests where the extension or filename suffix denotes that it is a test.
+					'**/*{.,_}{test,spec,stories}.{ts,tsx}',
+				],
+				optionalDependencies: false,
+			},
+		],
 	},
 	overrides: [
 		{
